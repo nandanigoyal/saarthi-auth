289c905fc17c5da2d6ca0d8581bf7f14edea6d04
NANDANI GOYAL <155057906+nandanigoyal@users.noreply.github.com>
2026-08-03 23:06:43 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,8 @@ app.use(
       'http://localhost:8080',
       'http://127.0.0.1:8080',
       'http://localhost:5173',
-      'http://127.0.0.1:5173'
+      'http://127.0.0.1:5173',
+      'https://saarthi-empower-hub-revamp.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
